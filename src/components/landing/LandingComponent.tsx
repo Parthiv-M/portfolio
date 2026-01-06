e17b2d95bdf361f267f6d0b951454b1c4af4f32e
parthiv-m <parthivmenon.dev@gmail.com>
2026-01-05 23:10:55 -0500

    Modify landing component

--- src/components/landing/LandingComponent.tsx
+++ src/components/landing/LandingComponent.tsx
@@ -8,15 +8,15 @@ export const LandingComponent = () => {
     const data = usePortfolioData();
 
     return (
-        <div className="h-[200vh]">
-            <div className='h-5/11 grid grid-cols-12 gap-2'>
+        <div className="mb-2">
+            <div className='h-[calc(100vh-15vh)] grid grid-cols-12 gap-2'>
                 <div className='col-span-8 border rounded-md flex items-end justify-center'>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img src="/ppixel.png" alt='Parthiv Menon' width={400} />
                 </div>
                 <div className='col-span-4 border rounded-md'><Bio /></div>
             </div>
-            <div className='h-6/11 border rounded-md mt-2'></div>
+            <div className='h-screen border rounded-md mt-2'></div>
         </div>
     );
 };
