0e363ec95283bfa2c49d1e4217fb128f9844949a
parthiv-m <parthivmenon.dev@gmail.com>
2026-01-11 00:23:32 -0500

    Add more detail to projects in expanded view

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,27 +1,79 @@
+'use client'
+
 import { Project } from "@/types";
-import { Maximize2 } from "@deemlol/next-icons";
+import { Maximize2, Minimize2 } from "@deemlol/next-icons";
+import { useState } from "react";
 
 interface ProjectCardProps extends Project {
     classes?: string
 }
 
 export default function ProjectCard(props: ProjectCardProps) {
+    const [isExpanded, setExpanded] = useState(false);
     return (
-        <div className={`col-span-12 md:col-span-${props?.colSpan} flex flex-col justify-between border rounded-md p-4 sm:p-6 ${props?.classes}`}>
-            <div>
-                <h6 className="text-2xl sm:text-3xl md:text-4xl font-bold">{props.name}</h6>
-                <p className="mt-4 text-sm sm:text-base">
-                    {props?.description}
-                </p>
-            </div>
-            <div className="flex justify-between mt-6">
+        <>
+            {/* init state */}
+            <div className={`
+                ${isExpanded && "absolute h-full w-full overflow-hidden bg-[#F0ECE3]"}
+                col-span-12 md:col-span-${props?.colSpan}
+                flex flex-col justify-between border rounded-md p-4 sm:p-6 ${props?.classes}
+            `}>
                 <div>
-                    {/* put tech stack icons here, visible on hover? */}
+                    <h6 className="text-2xl sm:text-3xl md:text-4xl font-bold">{props.name}</h6>
+                    <p className="mt-4 text-sm sm:text-base">
+                        {props?.description}
+                    </p>
                 </div>
-                <div>
-                    <Maximize2 size={24} />
+                {isExpanded &&
+                    <div className="grid grid-cols-12 h-full mt-4">
+                        <div className="absolute -bottom-8 w-2/3 col-span-8 self-end">
+                            <img src={props.image || "/projects/image.png"} alt={props.name} className="w-full rounded-t-lg grayscale" />
+                            <div className="absolute top-0 h-full w-full bg-gradient-to-t from-black to-transparent via-transparent rounded-t-lg"></div>
+                        </div>
+                        <div className="col-span-8"></div>
+                        <div className="col-span-4 px-12 py-2 w-full h-full bg-red00">
+                            {props.purpose && (
+                                <>
+                                    <h6 className="text-lg font-bold">Purpose</h6>
+                                    <p>{props.purpose}</p>
+                                </>
+                            )}
+                            {props.technicalities && (
+                                <>
+                                    <h6 className="text-lg font-bold mt-8">Technicalities</h6>
+                                    <p className="mt-1">{props.technicalities}</p>
+                                </>
+                            )}
+                            {props.other && (
+                                <>
+                                    <h6 className="text-lg font-bold mt-8">Other stuff</h6>
+                                    <p className="mt-1">{props.other}</p>
+                                </>
+                            )}
+                            {props.links && props.links.length > 0 && (
+                                <>
+                                    <h6 className="text-lg font-bold mt-8">Links</h6>
+                                    <div className="mt-1 flex flex-col gap-1">
+                                        {props.links.map((link, idx) => (
+                                            <a key={idx} href={link.url} target="_blank" rel="noopener noreferrer" className="underline">
+                                                {link.label}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </div>}
+                <div className="flex justify-between mt-6">
+                    <div>
+                        {/* put tech stack icons here, visible on hover? */}
+                    </div>
+                    <div className="cursor-pointer rounded-full hover:bg-black/10 p-2">
+                        {!isExpanded && <Maximize2 size={24} onClick={() => setExpanded(true)} />}
+                        {isExpanded && <Minimize2 size={24} onClick={() => setExpanded(false)} />}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/components/projects/Section.tsx
+++ src/components/projects/Section.tsx
@@ -7,7 +7,7 @@ export default function ProjectSection() {
     const { projects } = usePortfolioData();
     return (
         <Section>
-            <div className="min-h-[95vh] grid grid-cols-1 md:grid-cols-12 md:grid-rows-3 gap-2 mt-2">
+            <div className="min-h-screen grid grid-cols-1 md:grid-cols-12 md:grid-rows-3 gap-2 mt-2">
                 {
                     projects.map((project: Project) => <ProjectCard key={project.name} {...project} />)
                 }
