a9f2a3937d1fff7c5f0974d448d71fe795cb9f1c
parthiv-m <parthivmenon.dev@gmail.com>
2026-01-17 22:31:20 -0500

    Improve responsive behaviour of landing page

--- src/components/landing/Bio.tsx
+++ src/components/landing/Bio.tsx
@@ -6,14 +6,14 @@ export default function Bio() {
     return (
         <div className="h-full p-6 sm:p-10 flex flex-col justify-between">
             <div>
-                <h1 className="antic text-6xl sm:text-7xl md:text-8xl">{data.name.split(" ")[0]}</h1>
-                <h1 className="antic text-6xl sm:text-7xl md:text-8xl">{data.name.split(" ")[1]}</h1>
+                <h1 className="antic text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl tracking-tight">{data.name.split(" ")[0]}</h1>
+                <h1 className="antic text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl tracking-tight">{data.name.split(" ")[1]}</h1>
                 <div className="mt-6 sm:mt-8">
                     <h4 className="text-lg sm:text-xl">Software Engineer /</h4>
                     <h4 className="text-lg sm:text-xl">Open Source Enthusiast</h4>
                 </div>
                 <div className="mt-6 sm:mt-8">
-                    <p className="text-lg sm:text-xl">
+                    <p className="text-base sm:text-lg md:text-md">
                         Between finding the next best alliteration and fixing the syntactical
                         nitty gritties of my latest code lies the crux of my personality.
                     </p>
--- src/components/landing/LandingComponent.tsx
+++ src/components/landing/LandingComponent.tsx
@@ -6,11 +6,11 @@ export const LandingComponent = () => {
     return (
         <div className="mb-2">
             <div className='min-h-[calc(100vh-15vh)] flex flex-col md:grid md:grid-cols-12 gap-2'>
-                <div className='flex-1 md:col-span-8 border rounded-md min-h-[400px] md:min-h-0'>
+                <div className='flex-1 md:col-span-6 lg:col-span-8 border rounded-md min-h-[400px] md:min-h-0'>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <Photo />
                 </div>
-                <div className='md:col-span-4 border rounded-md'><Bio /></div>
+                <div className='flex-1 md:col-span-6 lg:col-span-4 border rounded-md'><Bio /></div>
             </div>
             <div className='h-[50vh] md:h-screen border rounded-md mt-2'>
                 <Stack />
